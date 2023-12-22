6d4f7ad362eb1f0cf56cb33b38d4e8d65b3cb35c
Sferra <105938333+FarInHeight@users.noreply.github.com>
2023-12-22 18:29:46 +0100

    modified just a print in main file

--- main.py
+++ main.py
@@ -10,10 +10,10 @@ if __name__ == '__main__':
     # player1 = AlphaBetaMinMaxPlayer(0, depth=4)
     player1 = RandomPlayer()
     # player2 = RandomPlayer()
-    player2 = AlphaBetaMinMaxPlayer(1, depth=4, symmetries=True)
+    player2 = AlphaBetaMinMaxPlayer(1, depth=5, symmetries=True)
     start = time.time()
     winner = g.play(player1, player2)
     total_time = time.time() - start
     g.print()
     print(f"Winner: Player {winner}")
-    print(f'Game duration: {total_time:.2f} sec, {total_time / 60:.2f} min')
+    print(f'Game duration: {total_time:.2E} sec, {total_time / 60:.2E} min')
