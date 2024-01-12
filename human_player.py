011951ed9e08c44105e7c3d19efc6bd590e270e8
Sferra <105938333+FarInHeight@users.noreply.github.com>
2024-01-12 08:37:18 +0100

    added comments to the human player

--- human_player.py
+++ human_player.py
@@ -2,37 +2,82 @@ from game import Game, Move, Player
 
 
 class HumanPlayer(Player):
+    """
+    Class that allows a human being to play.
+    """
+
     def __init__(self) -> None:
+        """
+        Constructor of the human player.
+
+        Args:
+            None.
+
+        Returns:
+            None.
+        """
         super().__init__()
-        self._slides = {"r": Move.RIGHT, "l": Move.LEFT, "t": Move.TOP, "b": Move.BOTTOM}
+        self.__slides = {"r": Move.RIGHT, "l": Move.LEFT, "t": Move.TOP, "b": Move.BOTTOM}
 
     def make_move(self, game: 'Game') -> tuple[tuple[int, int], Move]:
+        """
+        Play a valid move.
+
+        Args:
+            game: a game instance.
+
+        Returns:
+            A move to play is returned.
+        """
+        # set the move as invalid
         valid = False
+        # while the chosen move is not valid
         while not valid:
-            valid, action = self._get_input(game)
+            # let the user choose the move
+            valid, action = self.__get_input(game)
+        # return the chosen move
         return action
 
-    def _get_input(self, game: 'Game'):
-        baord = game.get_board()
+    def __get_input(self, game: 'Game'):
+        # get the current board
+        board = game.get_board()
+        # set the move as invalid
         valid, action = False, None
         try:
+            # let the user decide the x-value
             x = int(input('Choose a position in the x-axis (→) in [0-4]: '))
-            if x < 0 or x >= baord.shape[1]:
+            # if the x-value is not valid
+            if x < 0 or x >= board.shape[1]:
+                # print a descriptive message
                 print('Invalid integer value. Please insert an integer between 0 and 4.')
+                # return an invalid move
                 valid, action = False, None
             else:
+                # let the user decide the y-value
                 y = int(input('Choose a position in the y-axis (↓) in [0-4]: '))
-                if y < 0 or y >= baord.shape[0]:
+                # if the y-value is not valid
+                if y < 0 or y >= board.shape[0]:
+                    # print a descriptive message
                     print('Invalid integer value. Please insert an integer between 0 and 4.')
+                    # return an invalid move
                     valid, action = False, None
+                # otherwise
                 else:
-                    slide = self._slides[input('Choose a slide in {"r", "l", "t", "b"}: ')]
+                    # let the user decide a slide
+                    slide = self.__slides[input('Choose a slide in {"r", "l", "t", "b"}: ')]
+                    # save the chosen move
                     valid, action = True, ((x, y), slide)
+        # if the user chooses a non-integer value of x or y
         except ValueError:
+            # print a descriptive message
             print('Invalid non-integer value.')
+        # if the user chooses an invalid slide
         except KeyError:
+            # print a descriptive message
             print('Invalid slide value.')
+        # if the user's input is ok
         finally:
+            # return the chosen move
             return valid, action
 
 
